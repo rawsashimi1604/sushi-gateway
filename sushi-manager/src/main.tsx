d15b19140faff6b86b7eeb02addf608afddd9a6b
rawsashimi1604 <looweiren@gmail.com>
2024-01-19 19:03:59 +0800

    init sushi-manager with tailwind

---
+++ sushi-manager/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <h1 className="text-red-500">hello world</h1>
+  </React.StrictMode>
+);
