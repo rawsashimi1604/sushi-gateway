3514cb74539c2230d6b9adfb9a97e07996cc6672
rawsashimi1604 <looweiren@gmail.com>
2024-01-26 17:25:15 +0800

    added postgres to dockercompose

---
+++ sushi-proxy/cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Run checks to see if api req is present in ingress
+
+	// Run through auth
+
+	// Delegate to plugin runner
+
+	// Run through other plugins
+
+	fmt.Println("hello world!")
+}
