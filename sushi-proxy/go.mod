073db1c1cd1c110a9abf5e18b64debc4b3e16465
rawsashimi1604 <looweiren@gmail.com>
2024-02-05 15:45:17 +0800

    added config file for sushi-proxy

--- sushi-proxy/go.mod
+++ sushi-proxy/go.mod
@@ -1,3 +1,5 @@
 module github.com/rawsashimi1604/sushi-gateway
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
---
+++ sushi-proxy/internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"errors"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	REVERSE_PROXY_HTTP_URL  string
+	REVERSE_PROXY_HTTPS_URL string
+}
+s
+func LoadConfig() (*AppConfig, error) {
+	godotenv.Load()
+
+	revProxyHttpUrl := os.Getenv("REVERSE_PROXY_HTTP_URL")
+	if revProxyHttpUrl == "" {
+		return nil, errors.New("REVERSE_PROXY_HTTP_URL is required")
+	}
+
+	revProxyHttpsUrl := os.Getenv("REVERSE_PROXY_HTTPS_URL")
+	if revProxyHttpsUrl == "" {
+		return nil, errors.New("REVERSE_PROXY_HTTPS_URL is required")
+	}
+
+	config := &AppConfig{
+		REVERSE_PROXY_HTTP_URL:  revProxyHttpUrl,
+		REVERSE_PROXY_HTTPS_URL: revProxyHttpsUrl,
+	}
+
+	return config, nil
+}
