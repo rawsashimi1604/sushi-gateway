cc2029f49b0331b9fbc9237b9c01c79230c358c8
rawsashimi1604 <looweiren@gmail.com>
2024-02-21 19:02:50 +0800

    update db configuration and added simple service seeding

--- docker/sql-scripts/init.sql
+++ docker/sql-scripts/init.sql
@@ -1,16 +1,17 @@
 \c sushi-db;
 
-CREATE TABLE apis (
+CREATE TABLE services (
     id UUID PRIMARY KEY,
     created_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ,
-    name TEXT,
+    name TEXT UNIQUE,
     host TEXT,
     port BIGINT,
+    protocol TEXT,
     tags TEXT[],
-    enabled BOOLEAN,
+    enabled BOOLEAN DEFAULT true,
     health_check_enabled BOOLEAN,
-    health TEXT
+    health BOOLEAN
 );
 
 CREATE TABLE routes (
@@ -18,9 +19,27 @@ CREATE TABLE routes (
     created_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ,
     name TEXT,
-    service_id UUID REFERENCES apis(id),
+    path TEXT,
+    service_id UUID REFERENCES services(id),
     methods TEXT[],
     tags TEXT[]
 );
+
+DO $$
+DECLARE
+   service_1_uuid uuid = gen_random_uuid();
+   service_2_uuid uuid = gen_random_uuid();
+BEGIN
+INSERT INTO services(id, created_at, updated_at, name, host, port, protocol) VALUES
+    (service_1_uuid, current_timestamp, current_timestamp, 'sushi-test-service-1', 'localhost', 8001, 'http'),
+    (service_2_uuid, current_timestamp, current_timestamp, 'sushi-test-service-2', 'localhost', 8002, 'http');
+
+INSERT INTO routes(id, created_at, updated_at, name, path, service_id, methods) VALUES
+    (gen_random_uuid(), current_timestamp, current_timestamp, 'List Sushi', '/v1/sushi', service_1_uuid, ARRAY['GET']),
+    (gen_random_uuid(), current_timestamp, current_timestamp, 'List Sushi', '/v1/sushi', service_2_uuid, ARRAY['GET']),
+    (gen_random_uuid(), current_timestamp, current_timestamp, 'List Sushi Restaurants', '/v1/sushi/restaurant', service_1_uuid, ARRAY['GET']),
+    (gen_random_uuid(), current_timestamp, current_timestamp, 'List Sushi Restaurants', '/v1/sushi/restaurant', service_2_uuid, ARRAY['GET']);
+END $$
+
 
 
