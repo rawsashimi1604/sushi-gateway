e338b99dcfe7310a2dcf1ae2e70c1a72f6fd2347
rawsashimi1604 <looweiren@gmail.com>
2024-02-24 16:01:28 +0800

    update docs

---
+++ sushi-proxy/internal/gateway/service.go
@@ -0,0 +1,12 @@
+package gateway
+
+type ServiceManager struct{}
+
+func (sm *ServiceManager) NewServiceManager() {
+}
+
+// AddService adds a new service to the gateway using HaProxy
+// dataplane api
+func (sm *ServiceManager) AddService() {
+
+}
