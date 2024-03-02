f6913049c722d3bc10cd87eba27571733666bf17
Gavin Loo <looweiren@gmail.com>
2024-03-03 01:19:20 +0800

    clean up rate limit code

--- sushi-proxy/internal/plugins/rate_limit/rate_limit.go
+++ sushi-proxy/internal/plugins/rate_limit/rate_limit.go
@@ -1,7 +1,7 @@
 package rate_limit
 
 import (
-	plugins2 "github.com/rawsashimi1604/sushi-gateway/sushi-proxy/internal/plugins"
+	"github.com/rawsashimi1604/sushi-gateway/sushi-proxy/internal/plugins"
 	"log/slog"
 	"net/http"
 )
@@ -13,17 +13,14 @@ var Plugin = NewRateLimitPlugin()
 func (plugin RateLimitPlugin) Execute(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("Executing rate limit function...")
-		//slog.Info("Too many requests!!! Rate limit reached!!!")
-		//pluginErr := plugins2.NewPluginError(http.StatusBadRequest, "RATE_LIMIT_MOCK_ERROR", "rate limit reached!")
-		//pluginErr.WriteJSONResponse(w)
 
 		// call the next plugin.
 		next.ServeHTTP(w, r)
 	})
 }
 
-func NewRateLimitPlugin() *plugins2.Plugin {
-	return &plugins2.Plugin{
+func NewRateLimitPlugin() *plugins.Plugin {
+	return &plugins.Plugin{
 		Name:     "rate_limit",
 		Priority: 10,
 		Handler:  RateLimitPlugin{},
