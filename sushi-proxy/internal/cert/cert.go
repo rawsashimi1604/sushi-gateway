1198cf81de05947512a7faa7d3806fdd850b0c9d
Gavin Loo <looweiren@gmail.com>
2024-03-18 23:00:44 +0800

    fix syntax err

--- sushi-proxy/internal/cert/cert.go
+++ sushi-proxy/internal/cert/cert.go
@@ -3,7 +3,6 @@ package cert
 import (
 	"crypto/x509"
 	"github.com/rawsashimi1604/sushi-gateway/sushi-proxy/internal/config"
-	"io/ioutil"
 	"log"
 	"os"
 )
@@ -16,7 +15,7 @@ type CertPool struct {
 
 func LoadCertPool() *CertPool {
 	// Load CA certificate to create a CA pool
-	caCert, err := os.ReadFile((config.GlobalAppConfig.CACertPath)
+	caCert, err := os.ReadFile(config.GlobalAppConfig.CACertPath)
 	if err != nil {
 		log.Fatalf("server: read ca: %s", err)
 	}
