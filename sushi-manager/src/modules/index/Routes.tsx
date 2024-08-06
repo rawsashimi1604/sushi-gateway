4ff4b25dc8aa04d0b6bdb55b6f992063ecfac1c8
rawsashimi1604 <looweiren@gmail.com>
2024-08-07 00:57:33 +0800

    added no routes were found and no services were found text

--- sushi-manager/src/modules/index/Routes.tsx
+++ sushi-manager/src/modules/index/Routes.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Header from "../../components/typography/Header";
 import RouteDropdown from "../../components/sushi-gateway/RouteDropdown";
+import NormalText from "../../components/typography/Text";
 
 interface RoutesProps {
   data: any;
@@ -10,6 +10,9 @@ function Routes({ data }: RoutesProps) {
   return (
     <div>
       <Header text="Routes" align="left" size="md" />
+      {(data?.length === 0 || data === null) && (
+        <NormalText text="No routes were found." />
+      )}
       <div className="flex flex-col gap-3">
         {data?.map((route: any) => {
           return <RouteDropdown data={route} />;
--- sushi-manager/src/modules/index/Services.tsx
+++ sushi-manager/src/modules/index/Services.tsx
@@ -1,5 +1,6 @@
 import Header from "../../components/typography/Header";
 import ServiceDropdown from "../../components/sushi-gateway/ServiceDropdown";
+import NormalText from "../../components/typography/Text";
 
 interface ServiceProps {
   data: any;
@@ -9,6 +10,9 @@ function Services({ data }: ServiceProps) {
   return (
     <div>
       <Header text="Services" align="left" size="md" />
+      {(data?.length === 0 || data === null) && (
+        <NormalText text="No services were found." />
+      )}
       <div className="flex flex-col gap-3">
         {data?.map((service: any) => {
           return <ServiceDropdown key={service?.name} data={service} />;
