68aec836bf5f0377fc1a126cfb105bfbf635946c
rawsashimi1604 <looweiren@gmail.com>
2024-08-07 00:28:48 +0800

    added plugin names

--- sushi-manager/src/data/plugins.ts
+++ sushi-manager/src/data/plugins.ts
@@ -1,4 +1,11 @@
 export const plugins: { [key: string]: string } = {
   basic_auth: "Basic Authentication",
   http_log: "Http Log",
+  mtls: "Mutual TLS",
+  rate_limit: "Rate Limiting",
+  jwt: "JWT Authentication",
+  acl: "Access Control List",
+  bot_protection: "Bot Protection",
+  request_size_limit: "Request Size Limiting",
+  cors: "Cross Origin Resource Sharing",
 };
