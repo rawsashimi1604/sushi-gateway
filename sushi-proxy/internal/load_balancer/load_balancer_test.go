40423468a35a4899215c13d31066c4c01066f5bc
rawsashimi1604 <looweiren@gmail.com>
2024-08-14 14:54:07 +0800

    add load balancer round robin test

---
+++ sushi-proxy/internal/load_balancer/load_balancer_test.go
@@ -0,0 +1,50 @@
+package load_balancer
+
+import (
+	"github.com/rawsashimi1604/sushi-gateway/sushi-proxy/internal/models"
+	"testing"
+)
+
+// Write test to test load balancer given a service with multiple upstreams
+
+func TestRoundRobin(t *testing.T) {
+
+	Reset()
+	lb := NewLoadBalancer()
+	service := models.Service{
+		Name: "test",
+		Upstreams: []models.Upstream{
+			{
+				Host: "localhost",
+				Port: 8080,
+			},
+			{
+				Host: "localhost",
+				Port: 8081,
+			},
+			{
+				Host: "localhost",
+				Port: 8082,
+			},
+		},
+	}
+	firstReq := lb.handleRoundRobin(service)
+	secondReq := lb.handleRoundRobin(service)
+	thirdReq := lb.handleRoundRobin(service)
+
+	if firstReq != 0 {
+		t.Errorf("Expected 0, got %d", firstReq)
+	}
+	if secondReq != 1 {
+		t.Errorf("Expected 1, got %d", secondReq)
+	}
+	if thirdReq != 2 {
+		t.Errorf("Expected 2, got %d", thirdReq)
+	}
+
+	// Try the load balancer again it should round robin to first
+	fourthReq := lb.handleRoundRobin(service)
+	if fourthReq != 0 {
+		t.Errorf("Expected 0, got %d", fourthReq)
+	}
+}
