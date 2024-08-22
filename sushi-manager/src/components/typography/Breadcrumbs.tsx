d4029a03e52e7a772f84a36fe1807291b4c4d200
rawsashimi1604 <looweiren@gmail.com>
2024-08-22 14:15:37 +0800

    added github link

--- sushi-manager/src/components/typography/Breadcrumbs.tsx
+++ sushi-manager/src/components/typography/Breadcrumbs.tsx
@@ -1,3 +1,5 @@
+import { BsGithub } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
 
@@ -6,23 +8,31 @@ function Breadcrumbs() {
   const breadcrumbs = useBreadcrumbs();
 
   return (
-    <div className="flex flex-row items-center text-sm gap-1">
-      <span className="text-gray-500">gateway {">"}</span>
-      {breadcrumbs.map((breadcrumb, i) => {
-        return (
-          <span
-            key={i + (breadcrumb.breadcrumb?.toString() as string)}
-            className={`${
-              breadcrumb.key === location.pathname
-                ? "font-semibold"
-                : "text-gray-500"
-            }`}
-          >
-            {breadcrumb.key === "/" ? "manager" : breadcrumb.key.slice(1)}{" "}
-            {i + 1 !== breadcrumbs.length && ">"}
-          </span>
-        );
-      })}
+    <div className="flex items-center justify-between w-full">
+      <div className="flex flex-row items-center text-sm gap-1">
+        <span className="text-gray-500">gateway {">"}</span>
+        {breadcrumbs.map((breadcrumb, i) => {
+          return (
+            <span
+              key={i + (breadcrumb.breadcrumb?.toString() as string)}
+              className={`${
+                breadcrumb.key === location.pathname
+                  ? "font-semibold"
+                  : "text-gray-500"
+              }`}
+            >
+              {breadcrumb.key === "/" ? "manager" : breadcrumb.key.slice(1)}{" "}
+              {i + 1 !== breadcrumbs.length && ">"}
+            </span>
+          );
+        })}
+      </div>
+      <Link
+        to="https://github.com/rawsashimi1604/sushi-gateway"
+        target="_blank"
+      >
+        <BsGithub className="cursor-pointer duration-150 transition-all hover:opacity-80" />
+      </Link>
     </div>
   );
 }
