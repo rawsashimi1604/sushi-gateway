4097ed56fb04b56daf23d9abfc2fee56d546b7f4
rawsashimi1604 <looweiren@gmail.com>
2024-09-04 14:54:55 +0800

    update logo

--- sushi-manager/src/components/sushi-gateway/Logo.tsx
+++ sushi-manager/src/components/sushi-gateway/Logo.tsx
@@ -1,19 +1,10 @@
 import { ReactComponent as LogoSvg } from "../../assets/sushi.svg";
+import logo from "../../assets/logos/sushi-manager.png";
 
 function Logo() {
   return (
-    <div className="flex flex-row justify-center items-center font-light text-xl font-sans gap-4">
-      <LogoSvg className="w-16 h-16" />
-      <div className="relative">
-        <h1 className="-mt-2 font-lora font-ligh tracking-widest">
-          SUSHI GATEWAY
-        </h1>
-        <div className="absolute right-0 top-5.5">
-          <span className="font-lora tracking-wider text-sm border border-black rounded-sm shadow-md px-2 py-0.5 pb-1 font-bold">
-            manager
-          </span>
-        </div>
-      </div>
+    <div className="w-60">
+      <img src={logo} />
     </div>
   );
 }
