888632af05f0161dc3d290546bae1cdac20aa94f
rawsashimi1604 <looweiren@gmail.com>
2024-09-04 14:57:14 +0800

    update logo size

--- sushi-manager/src/components/sushi-gateway/Logo.tsx
+++ sushi-manager/src/components/sushi-gateway/Logo.tsx
@@ -2,7 +2,7 @@ import logo from "../../assets/logos/sushi-manager.jpeg";
 
 function Logo() {
   return (
-    <div className="w-60">
+    <div className="w-72">
       <img src={logo} />
     </div>
   );
