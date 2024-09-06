ffae997fb9c1ecdab6711f5635158f75b290c11f
rawsashimi1604 <looweiren@gmail.com>
2024-09-06 15:25:51 +0800

    resized summary card

--- sushi-manager/src/modules/index/Summary.tsx
+++ sushi-manager/src/modules/index/Summary.tsx
@@ -9,36 +9,36 @@ function Summary() {
       <div className="flex items-center gap-8">
         {/* Services */}
         <div className="flex flex-col gap-2 border-r pr-8">
-          <div className="flex gap-3">
-            <Header text="services" size="xs" align="left" />
-            <IoMdInformationCircle className="text-xl mt-1" />
+          <div className="flex gap-3 items-start justify-start">
+            <h2 className="tracking-wider text-sm">services</h2>
+            <IoMdInformationCircle className="text-md mt-0.5" />
           </div>
-          <Header text="2" align="left" size="xxl" />
+          <Header text="2" align="left" size="lg" />
         </div>
 
         {/* Routes */}
         <div className="flex flex-col gap-2 border-r pr-8">
-          <div className="flex gap-3">
-            <Header text="routes" size="xs" align="left" />
-            <IoMdInformationCircle className="text-xl mt-1" />
+          <div className="flex gap-3 items-start justify-start">
+            <h2 className="tracking-wider text-sm">routes</h2>
+            <IoMdInformationCircle className="text-md mt-0.5" />
           </div>
-          <Header text="8" align="left" size="xxl" />
+          <Header text="8" align="left" size="lg" />
         </div>
 
         {/* Plugins */}
         <div className="flex flex-col gap-2 border-r pr-8">
-          <div className="flex gap-3">
-            <Header text="plugins" size="xs" align="left" />
-            <IoMdInformationCircle className="text-xl mt-1" />
+          <div className="flex gap-3 items-start justify-start">
+            <h2 className="tracking-wider text-sm">plugins</h2>
+            <IoMdInformationCircle className="text-md mt-0.5" />
           </div>
-          <Header text="15" align="left" size="xxl" />
+          <Header text="15" align="left" size="lg" />
         </div>
 
         {/* Last Updated */}
-        <div className="flex flex-col gap-2 ">
-          <div className="flex gap-3">
-            <Header text="last updated" size="xs" align="left" />
-            <IoMdInformationCircle className="text-xl mt-1" />
+        <div className="flex flex-col gap-2 items-start justify-start">
+          <div className="flex gap-3 items-start justify-start">
+            <h2 className="tracking-wider text-sm">last updated</h2>
+            <IoMdInformationCircle className="text-md mt-0.5" />
           </div>
           <Header text="24 Sept 2024 15:00:00" align="left" size="lg" />
         </div>
