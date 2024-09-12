12d225d504ba3989c65476238b686a4607f3d9f2
rawsashimi1604 <looweiren@gmail.com>
2024-09-12 15:46:14 +0800

    added config card

--- sushi-manager/src/modules/index/Configuration.tsx
+++ sushi-manager/src/modules/index/Configuration.tsx
@@ -16,7 +16,7 @@ function ConfigurationItem({ item, value, tooltip }: ConfigurationItemProps) {
         {tooltip && <IoMdInformationCircle className="text-md mt-0.5" />}
       </div>
       <span className="tracking-wider text-sm font-serif pr-4">::</span>
-      <span className="tracking-wide text-sm">{value}</span>
+      <span className="tracking-wide text-sm font-mono">"{value}"</span>
     </div>
   );
 }
@@ -25,10 +25,25 @@ function Configuration() {
   return (
     <DashboardCard className="flex flex-col gap-2 p-6 ">
       <div className="">
-        <Header text="configuration" align="left" size="sm" />
-        <div className="flex flex-col gap-2">
+        <Header text="environment configuration" align="left" size="sm" />
+        <div className="mt-4 flex flex-col gap-2">
+          <ConfigurationItem item="proxy_version" value="0.1" />
           <ConfigurationItem item="state" value="stateless" />
-          <ConfigurationItem item="version" value="0.0.1" />
+          <ConfigurationItem
+            item="config_file_path"
+            value="./config/config.json"
+          />
+          <ConfigurationItem
+            item="server_cert_path"
+            value="./config/server.crt"
+          />
+          <ConfigurationItem
+            item="server_key_path"
+            value="./config/server.key"
+          />
+          <ConfigurationItem item="ca_cert_path" value="./config/ca.crt" />
+          <ConfigurationItem item="admin_user" value="admin" />
+          <ConfigurationItem item="admin_password" value="changeme" />
           <ConfigurationItem item="config_format" value="json" />
         </div>
       </div>
