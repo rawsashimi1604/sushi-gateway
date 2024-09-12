120a4d7b5539cbe6cd67ddaf660f126056e208c0
rawsashimi1604 <looweiren@gmail.com>
2024-09-12 15:51:34 +0800

    added o ther configurations

--- sushi-manager/src/modules/index/Configuration.tsx
+++ sushi-manager/src/modules/index/Configuration.tsx
@@ -1,6 +1,7 @@
 import { IoMdInformationCircle } from "react-icons/io";
 import DashboardCard from "../../components/layout/DashboardCard";
 import Header from "../../components/typography/Header";
+import Tag from "../../components/typography/Tag";
 
 interface ConfigurationItemProps {
   item: string;
@@ -12,11 +13,11 @@ function ConfigurationItem({ item, value, tooltip }: ConfigurationItemProps) {
   return (
     <div className="flex flex-row">
       <div className="w-[150px] flex gap-3 items-start justify-start">
-        <h2 className="tracking-wider text-sm font-light">{item}</h2>
+        <h2 className="tracking-wider text-sm">{item}</h2>
         {tooltip && <IoMdInformationCircle className="text-md mt-0.5" />}
       </div>
       <span className="tracking-wider text-sm font-serif pr-4">::</span>
-      <span className="tracking-wide text-sm font-mono">"{value}"</span>
+      <Tag value={value || ""} className="tracking-wide text-sm font-mono" />
     </div>
   );
 }
@@ -28,7 +29,21 @@ function Configuration() {
         <Header text="environment configuration" align="left" size="sm" />
         <div className="mt-4 flex flex-col gap-2">
           <ConfigurationItem item="proxy_version" value="0.1" />
-          <ConfigurationItem item="state" value="stateless" />
+          <ConfigurationItem item="manager_url" value="http://localhost:5173" />
+          <ConfigurationItem
+            item="admin_api_url"
+            value="http://localhost:8081"
+          />
+          <ConfigurationItem
+            item="proxy_http_url"
+            value="http://localhost:8008"
+          />
+          <ConfigurationItem
+            item="proxy_https_url"
+            value="https://localhost:8443"
+          />
+          <ConfigurationItem item="proxy_version" value="0.1" />
+          <ConfigurationItem item="data_model" value="stateless" />
           <ConfigurationItem
             item="config_file_path"
             value="./config/config.json"
