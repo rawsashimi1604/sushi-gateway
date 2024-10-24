72bdac01220598f73ceab5af0965c6bd71067f36
rawsashimi1604 <looweiren@gmail.com>
2024-10-24 14:33:08 +0800

    add route boilerplate

--- sushi-proxy/internal/api/route.go
+++ sushi-proxy/internal/api/route.go
@@ -1,1 +1,35 @@
 package api
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type RouteController struct {
+}
+
+func NewRouteController() *RouteController {
+	return &RouteController{}
+}
+
+func (r *RouteController) RegisterRoutes(router *mux.Router) {
+	router.PathPrefix("/").Methods("GET").Handler(
+		ProtectRouteUsingJWT(r.AddRoute()))
+}
+
+func (r *RouteController) AddRoute() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		payload, _ := json.Marshal("hello")
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(payload)
+	}
+}
+
+func (r *RouteController) DeleteRoute() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		payload, _ := json.Marshal("hello")
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(payload)
+	}
+}
