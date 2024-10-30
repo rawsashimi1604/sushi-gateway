5579e931a5eb443cd5618835751a3698e8cdb261
rawsashimi1604 <looweiren@gmail.com>
2024-10-30 23:34:02 +0800

    remove TODO

--- sushi-proxy/cmd/main.go
+++ sushi-proxy/cmd/main.go
@@ -33,8 +33,8 @@ func main() {
 		}
 
 		// Thereafter we should run a cron job to sync the config from the db.
-		// TODO: externalize this interval in env variables.
-		gateway.StartProxyConfigCronJob(database, gateway.GlobalAppConfig.PersistenceSyncInterval)
+		gateway.StartProxyConfigCronJob(database,
+			gateway.GlobalAppConfig.PersistenceSyncInterval)
 	}
 
 	// DB LESS MODE run a thread to monitor the config file for changes and do an initial boot up...
