bec996a30cebf214f8da24367a72f6813fc131a4
rawsashimi1604 <looweiren@gmail.com>
2024-11-18 09:03:39 +0800

    add basic test for e2e sushi proxy

--- sushi-proxy/internal/gateway/sushi_proxy_test.go
+++ sushi-proxy/internal/gateway/sushi_proxy_test.go
@@ -1,3 +1,54 @@
 package gateway
 
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
 // TODO: complete a simple e2e test for gateway.
+func TestHandleProxyRequest(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/mockService/mockRoute", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := NewSushiProxy().RouteRequest()
+	handler.ServeHTTP(rr, req)
+}
+
+func Test_HandleServiceNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/mockService/mockRoute", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := NewSushiProxy().RouteRequest()
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusNotFound {
+		t.Errorf("Expected 404, got %d", rr.Code)
+	}
+
+	var response map[string]interface{}
+	err = json.Unmarshal(rr.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf("Failed to parse response body: %v", err)
+	}
+
+	slog.Info(rr.Body.String())
+
+	expectedCode := "SERVICE_NOT_FOUND"
+	if errorField, ok := response["error"].(map[string]interface{}); ok {
+		if message, ok := errorField["Code"].(string); !ok || message != expectedCode {
+			t.Errorf("Expected message '%s', got '%v'", expectedCode, message)
+		}
+	} else {
+		t.Errorf("Expected error field in response body")
+	}
+}
