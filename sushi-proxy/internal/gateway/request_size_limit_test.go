9c02b419482a60d775e7bad964daedd957303388
rawsashimi1604 <looweiren@gmail.com>
2024-12-30 16:37:08 +0800

    added test and validation for request_size_limit

--- sushi-proxy/internal/gateway/request_size_limit_test.go
+++ sushi-proxy/internal/gateway/request_size_limit_test.go
@@ -46,3 +46,64 @@ func TestRequestSizeLimitBypass(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
 	}
 }
+
+func TestRequestSizeLimitValidation(t *testing.T) {
+	tests := []struct {
+		name        string
+		config      map[string]interface{}
+		expectError bool
+	}{
+		{
+			name: "valid config",
+			config: map[string]interface{}{
+				"max_size": float64(1024 * 1024), // 1MB
+			},
+			expectError: false,
+		},
+		{
+			name:        "missing max_size",
+			config:      map[string]interface{}{},
+			expectError: true,
+		},
+		{
+			name: "invalid max_size type",
+			config: map[string]interface{}{
+				"max_size": "1024",
+			},
+			expectError: true,
+		},
+		{
+			name: "zero max_size",
+			config: map[string]interface{}{
+				"max_size": float64(0),
+			},
+			expectError: true,
+		},
+		{
+			name: "negative max_size",
+			config: map[string]interface{}{
+				"max_size": float64(-1024),
+			},
+			expectError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pluginConfig, err := CreatePluginConfigInput(tt.config)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			plugin := NewRequestSizeLimitPlugin(pluginConfig)
+			err = plugin.Validator.Validate()
+
+			if tt.expectError && err == nil {
+				t.Error("expected error but got none")
+			}
+			if !tt.expectError && err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+		})
+	}
+}
