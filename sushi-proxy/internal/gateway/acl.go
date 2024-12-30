b30de6e81c6f53a71c56c71b3eab630a7643dca8
rawsashimi1604 <looweiren@gmail.com>
2024-12-30 15:46:44 +0800

    added plugin verification for acl plugin

--- sushi-proxy/internal/gateway/acl.go
+++ sushi-proxy/internal/gateway/acl.go
@@ -21,7 +21,51 @@ func NewAclPlugin(config map[string]interface{}) *Plugin {
 		Handler: AclPlugin{
 			config: config,
 		},
+		Validator: AclPlugin{
+			config: config,
+		},
 	}
+}
+
+func (plugin AclPlugin) Validate() error {
+	whitelist, hasWhitelist := plugin.config["whitelist"]
+	blacklist, hasBlacklist := plugin.config["blacklist"]
+
+	// Check that only one list type exists
+	if hasWhitelist && hasBlacklist {
+		return fmt.Errorf("only one of whitelist or blacklist can be configured at a time")
+	}
+
+	if !hasWhitelist && !hasBlacklist {
+		return fmt.Errorf("either whitelist or blacklist must be configured")
+	}
+
+	// Validate the present list
+	if hasWhitelist {
+		whitelistArr, ok := whitelist.([]interface{})
+		if !ok {
+			return fmt.Errorf("whitelist must be an array of strings")
+		}
+		for _, ip := range whitelistArr {
+			if _, ok := ip.(string); !ok {
+				return fmt.Errorf("whitelist entries must be strings")
+			}
+		}
+	}
+
+	if hasBlacklist {
+		blacklistArr, ok := blacklist.([]interface{})
+		if !ok {
+			return fmt.Errorf("blacklist must be an array of strings")
+		}
+		for _, ip := range blacklistArr {
+			if _, ok := ip.(string); !ok {
+				return fmt.Errorf("blacklist entries must be strings")
+			}
+		}
+	}
+
+	return nil
 }
 
 func (plugin AclPlugin) Execute(next http.Handler) http.Handler {
@@ -32,28 +76,32 @@ func (plugin AclPlugin) Execute(next http.Handler) http.Handler {
 		clientIP := r.RemoteAddr
 		forwardedIP := r.Header.Get(constant.X_FORWARDED_FOR)
 
-		// Check if the IP is in the whitelist
-		if plugin.isWhitelisted(clientIP) || plugin.isWhitelisted(forwardedIP) {
+		// If whitelist exists, only allow whitelisted IPs
+		if _, exists := plugin.config["whitelist"]; exists {
+			if plugin.isWhitelisted(clientIP) || plugin.isWhitelisted(forwardedIP) {
+				next.ServeHTTP(w, r)
+				return
+			}
+			err := model.NewHttpError(http.StatusForbidden,
+				"ACCESS_DENIED", "Access Denied")
+			err.WriteJSONResponse(w)
+			return
+		}
+
+		// If blacklist exists, deny blacklisted IPs
+		if _, exists := plugin.config["blacklist"]; exists {
+			if plugin.isBlacklisted(clientIP) || plugin.isBlacklisted(forwardedIP) {
+				slog.Info(fmt.Sprintf("IP %s is blacklisted", clientIP))
+				err := model.NewHttpError(http.StatusForbidden,
+					"ACCESS_DENIED", "Access Denied")
+				err.WriteJSONResponse(w)
+				return
+			}
 			next.ServeHTTP(w, r)
 			return
 		}
 
-		if plugin.isBlacklisted(clientIP) {
-			slog.Info(fmt.Sprintf("IP %s is blacklisted", clientIP))
-			err := model.NewHttpError(http.StatusForbidden,
-				"ACCESS_DENIED", "Access Denied")
-			err.WriteJSONResponse(w)
-			return
-		}
-
-		if plugin.isBlacklisted(forwardedIP) {
-			slog.Info(fmt.Sprintf("IP %s is blacklisted", clientIP))
-			err := model.NewHttpError(http.StatusForbidden,
-				"ACCESS_DENIED", "Access Denied")
-			err.WriteJSONResponse(w)
-			return
-		}
-
+		// If neither list exists (shouldn't happen due to validation), allow the request
 		next.ServeHTTP(w, r)
 	})
 }
@@ -63,11 +111,22 @@ func ValidateACLPlugin(config map[string]interface{}) {
 }
 
 func (plugin AclPlugin) isWhitelisted(ip string) bool {
-	// TODO: add validation for this plugin in the gateway file
-	config := plugin.config
-	whitelist := util.ToStringSlice(config["whitelist"].([]interface{}))
+	if ip == "" {
+		return false
+	}
 
-	for _, whitelistedIP := range whitelist {
+	config := plugin.config
+	whitelist, exists := config["whitelist"]
+	if !exists {
+		return false
+	}
+
+	whitelistArr, ok := whitelist.([]interface{})
+	if !ok {
+		return false
+	}
+
+	for _, whitelistedIP := range util.ToStringSlice(whitelistArr) {
 		if ip == whitelistedIP {
 			return true
 		}
@@ -76,11 +135,22 @@ func (plugin AclPlugin) isWhitelisted(ip string) bool {
 }
 
 func (plugin AclPlugin) isBlacklisted(ip string) bool {
-	// TODO: add validation for this plugin in the gateway file
-	config := plugin.config
-	blacklist := util.ToStringSlice(config["blacklist"].([]interface{}))
+	if ip == "" {
+		return false
+	}
 
-	for _, blacklistedIP := range blacklist {
+	config := plugin.config
+	blacklist, exists := config["blacklist"]
+	if !exists {
+		return false
+	}
+
+	blacklistArr, ok := blacklist.([]interface{})
+	if !ok {
+		return false
+	}
+
+	for _, blacklistedIP := range util.ToStringSlice(blacklistArr) {
 		if ip == blacklistedIP {
 			return true
 		}
