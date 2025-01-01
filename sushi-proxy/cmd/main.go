86c4b59bb48f411acd4937538394362bae3d06a7
rawsashimi1604 <looweiren@gmail.com>
2025-01-01 22:10:10 +0800

    added error groups to gracefully shutdown the gateway

--- sushi-proxy/cmd/main.go
+++ sushi-proxy/cmd/main.go
@@ -22,139 +22,140 @@ func main() {
 	defer cancel()
 	errGroup, errGrpCtx := errgroup.WithContext(errGrpCtx)
 
+	// Initialize all servers and routers first
 	appRouter := gateway.NewRouter()
 
-	// In DB MODE, we run a thread to sync the config file from db.
+	// Initialize HTTP server
+	httpServer := &http.Server{
+		Addr:    ":" + constant.PORT_HTTP,
+		Handler: appRouter,
+	}
+
+	// Initialize HTTPS server
+	cert, err := tls.LoadX509KeyPair(gateway.GlobalAppConfig.ServerCertPath, gateway.GlobalAppConfig.ServerKeyPath)
+	if err != nil {
+		slog.Error("Failed to load TLS keys", "error", err)
+		log.Fatal(err)
+	}
+
+	// Load global CA Cert Pool, allowing clients to send CA certificate for authentication via MTLS
+	gateway.GlobalCaCertPool = gateway.LoadCertPool()
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientCAs:    gateway.GlobalCaCertPool.Pool,
+		ClientAuth:   tls.RequestClientCert,
+	}
+
+	httpsServer := &http.Server{
+		Addr:      ":" + constant.PORT_HTTPS,
+		Handler:   appRouter,
+		TLSConfig: tlsConfig,
+	}
+
+	// Initialize Admin API server
+	var adminApiRouter http.Handler
 	if gateway.GlobalAppConfig.PersistenceConfig == constant.DB_MODE {
 		database, err := db.ConnectDb()
 		if err != nil {
-			panic("unable to connect to database.")
+			slog.Error("Failed to connect to database", "error", err)
+
 		}
+		slog.Info("PersistenceConfig:: Starting gateway in DB mode.")
+		adminApiRouter = api.NewAdminApiRouter(database)
 
 		// Load the initial config file.
 		gateway.LoadProxyConfigFromDb(database)
 
 		// On first load, if the config file is empty, means that the config is not in sync...
-		// Means we could not get the config from the db
-		// We should therefore terminate the application.
 		if gateway.GlobalProxyConfig.Global.Name == "" {
 			slog.Info("Unable to sync config from database on first load. Terminating...")
-			panic("unable to sync config from database.")
+			log.Fatal("unable to sync config from database.")
 		}
 
 		// We run the cron job to sync the config from the configured db here.
-		// We don't add the goroutine to the error group, as db fail syncs should not stop the gateway,
-		// the gateway can still function with the data loaded in it's cache, just that it's not up to date.
+		// We don't add the goroutine to the error group, as db fail syncs should not stop the gateway
 		gateway.StartProxyConfigCronJob(database,
 			gateway.GlobalAppConfig.PersistenceSyncInterval)
+	} else {
+		slog.Info("PersistenceConfig:: Starting gateway in DB-less mode.")
+		adminApiRouter = api.NewAdminApiRouter(nil)
 	}
 
-	// DB LESS MODE run a thread to monitor the config file for changes and do an initial boot up...
-	if gateway.GlobalAppConfig.PersistenceConfig == constant.DBLESS_MODE {
-		gateway.LoadProxyConfigFromConfigFile(gateway.GlobalAppConfig.ConfigFilePath)
+	adminServer := &http.Server{
+		Addr:    ":" + constant.PORT_ADMIN_API,
+		Handler: adminApiRouter,
+	}
 
+	// DB LESS MODE: Initialize config file watcher
+	if gateway.GlobalAppConfig.PersistenceConfig == constant.DBLESS_MODE {
+		// Do the first load on the config file
+		if err := gateway.LoadProxyConfigFromConfigFile(gateway.GlobalAppConfig.ConfigFilePath); err != nil {
+			slog.Error("Failed to load initial config file", "error", err)
+		}
+
+		// Start the file watcher
 		errGroup.Go(func() error {
-			return gateway.WatchConfigFile(gateway.GlobalAppConfig.ConfigFilePath)
+			return gateway.WatchConfigFile(errGrpCtx, gateway.GlobalAppConfig.ConfigFilePath)
 		})
 	}
 
-	// Setup http server
+	// Start all servers concurrently
+	// Start HTTP server
 	errGroup.Go(func() error {
-		server := &http.Server{
-			Addr:    ":" + constant.PORT_HTTP,
-			Handler: appRouter,
-		}
-
 		slog.Info("Started sushi-proxy_pass http server on port: " + constant.PORT_HTTP)
 
 		// Graceful shutdown on context cancellation
 		go func() {
 			<-errGrpCtx.Done()
-			server.Shutdown(context.Background())
+			httpServer.Shutdown(context.Background())
+			slog.Info("Gracefully shutdown HTTP Server....")
 		}()
 
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
 			slog.Error("HTTP server failed", "error", err)
 			return err
 		}
 		return nil
 	})
 
-	// Setup https server
+	// Start HTTPS server
 	errGroup.Go(func() error {
-		cert, err := tls.LoadX509KeyPair(gateway.GlobalAppConfig.ServerCertPath, gateway.GlobalAppConfig.ServerKeyPath)
-		if err != nil {
-			slog.Error("Failed to load TLS keys", "error", err)
-			return err
-		}
-
-		// Load global CA Cert Pool, allowing clients to send CA certificate for authentication via MTLS
-		gateway.GlobalCaCertPool = gateway.LoadCertPool()
-
-		tlsConfig := &tls.Config{
-			Certificates: []tls.Certificate{cert},
-			ClientCAs:    gateway.GlobalCaCertPool.Pool,
-			ClientAuth:   tls.RequestClientCert,
-		}
-
-		server := &http.Server{
-			Addr:      ":" + constant.PORT_HTTPS,
-			Handler:   appRouter,
-			TLSConfig: tlsConfig,
-		}
-
 		slog.Info("Started sushi-proxy_pass https server on port: " + constant.PORT_HTTPS)
 
 		// Graceful shutdown on context cancellation
 		go func() {
 			<-errGrpCtx.Done()
-			server.Shutdown(context.Background())
+			httpsServer.Shutdown(context.Background())
+			slog.Info("Gracefully shutdown HTTPS Server....")
 		}()
 
-		if err := server.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
+		if err := httpsServer.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
 			slog.Error("HTTPS server failed", "error", err)
 			return err
 		}
 		return nil
 	})
 
-	// Setup admin api
+	// Start Admin API server
 	errGroup.Go(func() error {
-		var adminApiRouter http.Handler
-		if gateway.GlobalAppConfig.PersistenceConfig == constant.DB_MODE {
-			database, err := db.ConnectDb()
-			if err != nil {
-				slog.Error("Failed to connect to database", "error", err)
-				return err
-			}
-			slog.Info("PersistenceConfig:: Starting gateway in DB mode.")
-			adminApiRouter = api.NewAdminApiRouter(database)
-		} else {
-			slog.Info("PersistenceConfig:: Starting gateway in DB-less mode.")
-			adminApiRouter = api.NewAdminApiRouter(nil)
-		}
-
-		server := &http.Server{
-			Addr:    ":" + constant.PORT_ADMIN_API,
-			Handler: adminApiRouter,
-		}
-
 		slog.Info("Started admin API server on port: " + constant.PORT_ADMIN_API)
 
 		// Graceful shutdown on context cancellation
 		go func() {
 			<-errGrpCtx.Done()
-			server.Shutdown(context.Background())
+			adminServer.Shutdown(context.Background())
+			slog.Info("Gracefully shutdown Admin API Server....")
 		}()
 
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		if err := adminServer.ListenAndServe(); err != http.ErrServerClosed {
 			slog.Error("Admin API server failed", "error", err)
 			return err
 		}
 		return nil
 	})
 
-	// Wait for all servers and handle errors, if any goroutines in the error group encounters an error, we shutdown all other go routines.
+	// Wait for all servers and handle errors
 	if err := errGroup.Wait(); err != nil {
 		slog.Error("Server error detected, shutting down...", "error", err)
 		log.Fatal(err)
